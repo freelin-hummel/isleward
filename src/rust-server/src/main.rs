ab6bd24109d6a00b1adc48248201b936eea1ef65
Pierre Laas <pierrel@intenda.za.com>
2024-10-22 18:04:59 +0200

    chore: Remove commented ws code
    Need to get the server ready for prod. Websockets can be implemented
    later if http get cannot resolve the issue

--- src/rust-server/src/main.rs
+++ src/rust-server/src/main.rs
@@ -1,12 +1,4 @@
-// use axum::extract::ws::Message;
-// use axum::extract::ws::WebSocket;
-
-// use axum::extract::WebSocketUpgrade;
-// use axum::response::Response;
-// use axum::routing::get;
 use axum::Router;
-
-// use futures_util::StreamExt;
 
 use std::net::SocketAddr;
 use tower::ServiceBuilder;
@@ -29,8 +21,6 @@ async fn main() {
     let app = Router::new()
         .nest_service("/", ServeDir::new("../../client"))
         .nest("/server", server_router())
-        // .route("/socket.io", get(ws_handler))
-        // .route("/socket.io/", get(ws_handler))
         .layer(
             ServiceBuilder::new()
                 .layer(CompressionLayer::new())
@@ -63,20 +53,3 @@ fn mods_router() -> Router {
         .nest_service("/ui", ServeDir::new("../../server/mods/ui"))
         .nest_service("/audio", ServeDir::new("../../server/mods/audio"))
 }
-
-// async fn ws_handler(ws: WebSocketUpgrade) -> Response {
-//     ws.on_upgrade(handle_socket)
-// }
-
-// async fn handle_socket(mut socket: WebSocket) {
-//     let (mut ws_stream, _response) = tokio_tungstenite::connect_async(format!(
-//         "ws://localhost:4001/socket.io/?EIO=4&transport=websocket"
-//     ))
-//     .await
-//     .unwrap();
-//     let (server_write, server_read) = ws_stream.split();
-//     let (client_write, client_read) = socket.split();
-
-//     tokio::io::copy(&mut server_read, &mut client_write).await?;
-//     tokio::io::copy(&mut client_read, &mut server_write).await?;
-// }
