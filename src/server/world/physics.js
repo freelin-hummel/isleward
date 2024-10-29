d209e8824816b8f0b1ed7d3ec6d5628a54bf8499
Shaun <skichenbrand@gmail.com>
2024-10-29 07:50:53 +0200

    bug: Fixed object id's changing after they've already been added to physics

--- src/server/world/physics.js
+++ src/server/world/physics.js
@@ -82,16 +82,6 @@ module.exports = {
 
 		for (let id of idsOfCollidingObjects) {
 			const checkObj = objects.objects.find(f => f.id + '' === id);
-			if (!checkObj) {
-				console.log({
-					error: 'No object found for removeObject collision',
-					objName: obj.name,
-					x,
-					y,
-					toX,
-					toY
-				});
-			}
 
 			checkObj.collisionExit(obj); 
 			obj.collisionExit(checkObj);
