b27c4d8da32d4dda63d794d31cab32c773339d54
Shaun <skichenbrand@gmail.com>
2025-05-09 13:44:37 +0200

    bug #2098: Don't allow fish to be used to charge arcane idols

--- src/server/config/recipes/enchanting.js
+++ src/server/config/recipes/enchanting.js
@@ -174,19 +174,19 @@ module.exports = [{
 		info: 'Pick an epic or legendary item that shares at least one stat with 2 others',
 		withoutProps: ['eq', 'noDrop', 'noDestroy', 'effects', 'factions'],
 		checks: [
-			item => item.quality >= 3 && item.stats !== undefined && !item.effects && item.slot !== 'tool'
+			item => item.quality >= 3 && item.stats !== undefined && !item.effects && item.slot !== 'tool' && item.slot !== undefined
 		]
 	}, {
 		info: 'Pick an epic or legendary item that shares at least one stat with 2 others',
 		withoutProps: ['eq', 'noDrop', 'noDestroy', 'effects', 'factions'],
 		checks: [
-			item => item.quality >= 3 && item.stats !== undefined && !item.effects && item.slot !== 'tool'
+			item => item.quality >= 3 && item.stats !== undefined && !item.effects && item.slot !== 'tool' && item.slot !== undefined
 		]
 	}, {
 		info: 'Pick an epic or legendary item that shares at least one stat with 2 others',
 		withoutProps: ['eq', 'noDrop', 'noDestroy', 'effects', 'factions'],
 		checks: [
-			item => item.quality >= 3 && item.stats !== undefined && !item.effects && item.slot !== 'tool'
+			item => item.quality >= 3 && item.stats !== undefined && !item.effects && item.slot !== 'tool' && item.slot !== undefined
 		]
 	}],
 	craftAction: extract
