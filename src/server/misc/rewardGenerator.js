b4aae35769f53eddfadd775aaf0ba06f23a06e35
Shaun <skichenbrand@gmail.com>
2026-01-20 09:03:28 +0200

    modding #2129: Allow mods to take over login rewards and rebalance login rewards

--- src/server/misc/rewardGenerator.js
+++ src/server/misc/rewardGenerator.js
@@ -2,36 +2,46 @@ const defaultConfig = [{
 	name: 'Iron Bar',
 	sprite: [0, 0],
 	quality: 0,
-	chance: 15
+	chance: 100
 }, {
 	name: 'Cloth Scrap',
 	sprite: [0, 1],
 	quality: 0,
-	chance: 15
+	chance: 100
 }, {
 	name: 'Leather Scrap',
 	sprite: [0, 7],
 	quality: 0,
-	chance: 15
-}, {
-	name: 'Skyblossom',
-	sprite: [1, 2],
-	quality: 0,
-	chance: 8
+	chance: 100
 }, {
 	name: 'Common Essence',
 	sprite: [0, 2],
 	quality: 0,
-	chance: 5
+	chance: 35
 }, {
 	name: 'Magic Essence',
 	sprite: [0, 3],
 	quality: 1,
-	chance: 2
+	chance: 25
 }, {
 	name: 'Rare Essence',
 	sprite: [0, 4],
 	quality: 2,
+	chance: 15
+}, {
+	name: 'Epic Essence',
+	sprite: [0, 5],
+	quality: 3,
+	chance: 5
+}, {
+	name: 'Legendary Essence',
+	sprite: [0, 6],
+	quality: 4,
+	chance: 1
+}, {
+	name: 'Cerulean Pearl',
+	sprite: [11, 9],
+	quality: 3,
 	chance: 1
 }];
 
@@ -70,6 +80,21 @@ module.exports = (itemCount, useConfig) => {
 		} else
 			item.quantity += (pick.quantity || 1);
 	}
+
+	items.sort((a, b) => {
+		const ai = config.findIndex(c => c.name === a.name);
+		const bi = config.findIndex(c => c.name === b.name);
+
+		//If something isn't in config for some reason, push it to the end
+		if (ai === -1 && bi === -1)
+			return 0;
+		if (ai === -1)
+			return 1;
+		if (bi === -1)
+			return -1;
+
+		return ai - bi;
+	});
 
 	return items;
 };
