40303da9e4186e8583af5f99ececda72cbc25ac8
Shaun <skichenbrand@gmail.com>
2026-02-01 08:49:25 +0200

    bug: fixed a crash caused by a player finishing a spell cast in the same tick that they die

--- src/server/config/spells/spellTemplate.js
+++ src/server/config/spells/spellTemplate.js
@@ -157,9 +157,12 @@ module.exports = {
 			this.obj.syncer.set(false, null, 'casting', (action.castTimeMax - this.castTime) / action.castTimeMax);
 
 			if (!this.castTime) {
+				if (this.obj.player)
+					this.obj.player.die(action.target);
+
 				this.currentAction = null;
 
-				if (this.cast(action)) {
+				if (!this.obj.dead && this.castingInfo && this.cast(action)) {
 					this.consumeMana();
 					this.setCd(this.castingInfo);
 
